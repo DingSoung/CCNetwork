8f1d402eb01bcda225d45c3ae4540da09daa6c43
Ding Songwen <DingSoung@gmail.com>
2017-08-14 14:57:34 +0800

    更新 Reachalibity

--- Network/Reachability.swift
+++ Network/Reachability.swift
@@ -2,30 +2,28 @@
 //  Copyright © 2016年 DingSoung. All rights reserved.
 
 import Foundation
-
 import SystemConfiguration
-
 
 /*
  @objc enum ReachabilityStatus:Int {
-    case notReachable = 0
-    case reachableViaWiFi
-    case reachableViaWWAN
-}
-*/
+ case notReachable = 0
+ case reachableViaWiFi
+ case reachableViaWWAN
+ }
+ */
 
 @objc open class Reachability:NSObject {
     
-    public static let changedNotification = "kNetworkReachabilityChangedNotification"
+    public static let notification = "NetworkReachabilityChangedNotification"
     
     private var networkReachability:SCNetworkReachability?
     private var notifying: Bool = false
     
     public init?(hostName: String) {
-        networkReachability = SCNetworkReachabilityCreateWithName(kCFAllocatorDefault, hostName)
+        self.networkReachability = SCNetworkReachabilityCreateWithName(kCFAllocatorDefault, hostName)
         
         super.init()
-        if networkReachability == nil {
+        if self.networkReachability == nil {
             return nil
         }
     }
@@ -43,17 +41,17 @@ import SystemConfiguration
         }
         
         var address = ipAddress
-        networkReachability = RouteReachability(&address)
-        if (networkReachability != nil) {
+        self.networkReachability = RouteReachability(&address)
+        if (self.networkReachability != nil) {
         } else {
             var zeroAddress = sockaddr_in()
             zeroAddress.sin_len = UInt8(MemoryLayout.size(ofValue: zeroAddress))
             zeroAddress.sin_family = sa_family_t(AF_INET)
-            networkReachability = RouteReachability(&zeroAddress)
+            self.networkReachability = RouteReachability(&zeroAddress)
         }
         
         super.init()
-        if networkReachability == nil {
+        if self.networkReachability == nil {
             return nil
         }
     }
@@ -63,38 +61,39 @@ import SystemConfiguration
     }
     
     deinit {
-        stopNotifier()
+        self.stopNotifier()
     }
     
-    open func startNotifier() -> Bool {
+    @discardableResult open func startNotifier() -> Bool {
         guard notifying == false else {
             return false
         }
         var context = SCNetworkReachabilityContext()
         context.info = UnsafeMutableRawPointer(Unmanaged.passUnretained(self).toOpaque())
-        guard let reachability = networkReachability,
+        guard let reachability = self.networkReachability,
             SCNetworkReachabilitySetCallback(reachability, { (target: SCNetworkReachability, flags: SCNetworkReachabilityFlags, info: UnsafeMutableRawPointer?) in
-            if let currentInfo = info {
-                let infoObject = Unmanaged<AnyObject>.fromOpaque(currentInfo).takeUnretainedValue()
-                if infoObject is Reachability {
-                    let networkReachability = infoObject as! Reachability
-                    NotificationCenter.default.post(name: Notification.Name(rawValue: Reachability.changedNotification), object: networkReachability)
+                if let currentInfo = info,
+                    let networkReachability = Unmanaged<AnyObject>.fromOpaque(currentInfo).takeUnretainedValue() as? Reachability {
+                    
+                    NotificationCenter.default.post(name: Notification.Name(rawValue: Reachability.notification), object: networkReachability)
                 }
-            }
-        }, &context) == true else {
-            return false
+            }, &context) == true else {
+                return false
         }
-        guard SCNetworkReachabilityScheduleWithRunLoop(reachability, CFRunLoopGetCurrent(), CFRunLoopMode.defaultMode.rawValue) == true else {
-            return false
+        
+        guard SCNetworkReachabilityScheduleWithRunLoop(reachability,
+                                                       CFRunLoopGetCurrent(),
+                                                       CFRunLoopMode.defaultMode.rawValue) == true else {
+                                                        return false
         }
-        notifying = true
-        return notifying
+        self.notifying = true
+        return self.notifying
     }
     
     open func stopNotifier() {
-        if let reachability = networkReachability, notifying == true {
+        if let reachability = self.networkReachability, self.notifying == true {
             SCNetworkReachabilityUnscheduleFromRunLoop(reachability, CFRunLoopGetCurrent(), CFRunLoopMode.defaultMode as! CFString)
-            notifying = false
+            self.notifying = false
         }
     }
     
@@ -109,18 +108,15 @@ import SystemConfiguration
         }
     }
     
-    /*
     open var reachable:Bool {
         // The target host is not reachable.
         if (currentReachabilityFlags.contains(.reachable) == false) {
             return false;
         }
-        
         // If the target host is reachable and no connection is required then we'll assume (for now) that you're on Wi-Fi...
         if currentReachabilityFlags.contains(.connectionRequired) == false {
             return true //Wi-Fi
         }
-        
         // ... and the connection is on-demand (or on-traffic) if the calling application is using the CFSocketStream or higher APIs...
         if currentReachabilityFlags.contains(.connectionOnDemand) == true || currentReachabilityFlags.contains(.connectionOnTraffic) == true {
             //... and no [user] intervention is needed...
@@ -128,15 +124,12 @@ import SystemConfiguration
                 return true //Wi-Fi
             }
         }
-        
         // ... but WWAN connections are OK if the calling application is using the CFNetwork APIs.
         if currentReachabilityFlags.contains(.isWWAN) == true {
             return true //WWAN
         }
-        
         return false
     }
-     */
     
     open var connectionRequired:Bool {
         guard let reach = self.networkReachability else {
