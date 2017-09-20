6a04923f13e75162eceebc1c19b42afe17341f13
Ding Songwen <DingSoung@gmail.com>
2017-09-20 17:15:38 +0800

    remove dependence , swift package menager

--- Package.swift
+++ Package.swift
@@ -1,10 +1,5 @@
-// swift-tools-version:3.1
-
 import PackageDescription
 
 let package = Package(
     name: "Network",
-    dependencies: [
-        .Package(url: "https://github.com/DingSoung/Extension.git", "0.6.0")
-    ]
 )
