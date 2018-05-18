b1789f8c3f6b022e6c9a52429d690c9c2ea96a8c
Songwen Ding <songwen@SongwendeMacBook-Pro.local>
2018-05-18 16:31:12 +0800

    feat: support custom contentType
    Change-Id: I201b490ba93bbdf1ae4da3feea34e2d911695bc8

--- Classes/URLRequest+Init.swift
+++ Classes/URLRequest+Init.swift
@@ -5,42 +5,55 @@ import Foundation
 
 extension URLRequest {
     public enum HTTPMethod: Int {
-        case options = 0, get, head, post, put, patch, delete, trace, connect
+        case get = 0, head, delete, post
+    }
+    public enum ContentType: Int {
+        case www_form_urlencoded = 0, json
     }
 }
 
 extension URLRequest.HTTPMethod {
     init?(rawString: String) {
         switch rawString {
-        case "OPTIONS": self = .options
         case "GET": self = .get
         case "HEAD": self = .head
-        case "POST": self = .post
-        case "PUT": self = .put
-        case "PATCH": self = .patch
         case "DELETE": self = .delete
-        case "TRACE": self = .trace
-        case "CONNECT": self = .connect
-        default: return nil
+        case "POST": self = .post
+        default:
+            // unsupport type: "OPTIONS" "PUT" "PATCH" "TRACE" "CONNECT"
+            return nil
         }
     }
-    fileprivate var rawString: String {
+    var rawString: String {
         switch self {
-        case .options: return "OPTIONS"
         case .get: return "GET"
         case .head: return "HEAD"
-        case .post: return "POST"
-        case .put: return "PUT"
-        case .patch: return "PATCH"
         case .delete: return "DELETE"
-        case .trace: return "TRACE"
-        case .connect: return "CONNECT"
+        case .post: return "POST"
+        }
+    }
+}
+
+extension URLRequest.ContentType {
+    init?(rawString: String) {
+        switch rawString {
+        case "application/x-www-form-urlencoded": self = .www_form_urlencoded
+        case "application/json": self = .json
+        default:
+            // unsupport type "charset=utf-8", "multipart/form-data", "text/xml"
+            return nil
+        }
+    }
+    var rawString: String {
+        switch self {
+        case .www_form_urlencoded: return "application/x-www-form-urlencoded"
+        case .json: return "application/json"
         }
     }
 }
 
 extension URLRequest {
-    public init?(method: HTTPMethod, url: String, parameters: [String: Any]?) {
+    public init?(method: HTTPMethod, url: String, parameters: [String: Any]? = nil, contentType: ContentType? = nil) {
         guard let parameters = parameters else {
             self.init(method: method.rawString, url: url, body: nil)
             return
@@ -50,7 +63,7 @@ extension URLRequest {
             if parameters.isEmpty == false,
                 let u = URL(string: url),
                 var components = URLComponents(url: u, resolvingAgainstBaseURL: false) {
-                let encodedQuery = (components.percentEncodedQuery.map { $0 + "&" } ?? "") + URLRequest.query(parameters)
+                let encodedQuery = (components.percentEncodedQuery.map { $0 + "&" } ?? "") + parameters.query
                 components.percentEncodedQuery = encodedQuery
                 if let encodeUrl = components.url?.absoluteString {
                     self.init(method: method.rawString, url: encodeUrl, body: nil)
@@ -59,16 +72,15 @@ extension URLRequest {
             }
             self.init(method: method.rawString, url: url, body: nil)
         case .post:
+            let cType = contentType ?? .json
             let body: Data?
-            do {
-                body = try JSONSerialization.data(withJSONObject: parameters, options: [])
-            } catch let error {
-                debugPrint(error.localizedDescription, parameters.debugDescription)
-                body = URLRequest.query(parameters).data(using: .utf8, allowLossyConversion: false)
+            switch cType {
+            case .www_form_urlencoded:
+                body = parameters.www_form_urlencoded
+            case .json:
+                body = parameters.json
             }
-            self.init(method: method.rawString, url: url, body: body)
-        default:
-            self.init(method: method.rawString, url: url, body: nil)
+            self.init(method: HTTPMethod.post.rawString, url: url, body: body)
         }
     }
     public init?(method: String, url: String, body: Data?) {
@@ -81,9 +93,7 @@ extension URLRequest {
         self.allowsCellularAccess = true
         self.httpMethod = method
         [
-            // Content-Type and Accept below
-            // Content-Type will be set automatic
-            // "application/x-www-form-urlencoded", "charset=utf-8", "multipart/form-data", "application/json", "text/xml"
+            // Content-Type will be set automatic by data type
             // example:
             // "*/*": "Accept"
             // "application/x-www-form-urlencoded": "Content-Type"
@@ -93,6 +103,28 @@ extension URLRequest {
         self.httpBody = body
         self.httpShouldHandleCookies = true
         self.httpShouldUsePipelining = true
+    }
+}
+
+extension Dictionary where Key == String {
+    public var query: String {
+        var components: [(String, String)] = []
+        for key in self.keys.sorted(by: <) {
+            let value = self[key]!
+            components += URLRequest.queryComponents(fromKey: key, value: value)
+        }
+        return components.map { "\($0)=\($1)" }.joined(separator: "&")
+    }
+    public var json: Data? {
+        do {
+            return try JSONSerialization.data(withJSONObject: self)
+        } catch let error {
+            debugPrint(error.localizedDescription, self.debugDescription)
+            return nil
+        }
+    }
+    public var www_form_urlencoded: Data? {
+        return self.query.data(using: .utf8, allowLossyConversion: false)
     }
 }
 
@@ -124,7 +156,7 @@ extension URLRequest {
         }
         return escaped
     }
-    private static func queryComponents(fromKey key: String, value: Any) -> [(String, String)] {
+    static func queryComponents(fromKey key: String, value: Any) -> [(String, String)] {
         var components: [(String, String)] = []
         if let dictionary = value as? [String: Any] {
             for (nestedKey, value) in dictionary {
@@ -146,13 +178,5 @@ extension URLRequest {
             components.append((escape(key), escape("\(value)")))
         }
         return components
-    }
-    private static func query(_ parameters: [String: Any]) -> String {
-        var components: [(String, String)] = []
-        for key in parameters.keys.sorted(by: <) {
-            let value = parameters[key]!
-            components += queryComponents(fromKey: key, value: value)
-        }
-        return components.map { "\($0)=\($1)" }.joined(separator: "&")
     }
 }
