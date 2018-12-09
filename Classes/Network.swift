1ec37e1c0cbbe7382b74457232ceabdea416c218
Songwen Ding <songwen@SongwendeMacBook-Pro.local>
2018-12-09 11:52:35 +0800

    chore: private ssl
    Change-Id: Ia902febf86c8be018e69100924b64f91b50757d9

--- Classes/Network.swift
+++ Classes/Network.swift
@@ -9,20 +9,23 @@ import Foundation
     deinit {}
 
     public lazy var session: URLSession = {
-        return URLSession(configuration: self.sessionConfiguration, delegate: self, delegateQueue: self.completionQueue)
+        return URLSession(configuration: sessionConfiguration, delegate: sessionDelegate, delegateQueue: completionQueue)
     }()
-    open var sslPinning: Data?
+    private var sessionDelegate = SessionDelegate()
+    public var sslPinning: Data? {
+        set { sessionDelegate.sslPinning = newValue }
+        get { return sessionDelegate.sslPinning }
+    }
     public lazy var completionQueue: OperationQueue = {
         return OperationQueue.main
     }()
-    open lazy var sessionConfiguration: URLSessionConfiguration = {
+    public lazy var sessionConfiguration: URLSessionConfiguration = {
         return URLSessionConfiguration.configuration(timeout: 60,
                                                      httpHeaders: ["Accept-Language": Locale.preferredLanguages.first ?? Locale.current.identifier])
     }()
 }
 
 extension Network {
-    // TODO: create functiuon to generate request, manage request
     @discardableResult public class func json<T>(request: URLRequest,
                                                  queue: OperationQueue = Network.shared.completionQueue,
                                                  trasnform: @escaping ([String: Any]) -> T?,
@@ -37,37 +40,41 @@ extension Network {
     }
 }
 
-extension Network: URLSessionDelegate {
-    public func urlSession(_ session: URLSession, didBecomeInvalidWithError error: Error?) {}
-    public func urlSession(_ session: URLSession, didReceive challenge: URLAuthenticationChallenge, completionHandler: @escaping (URLSession.AuthChallengeDisposition, URLCredential?) -> Swift.Void) {
-        guard let serverTrust = challenge.protectionSpace.serverTrust else {
-            completionHandler(URLSession.AuthChallengeDisposition.cancelAuthenticationChallenge, nil)
-            return
-        }
-        if let localCert = self.sslPinning, let secCertificate = SecTrustGetCertificateAtIndex(serverTrust, 0) {
-            // Set SSL policies for domain name check
-            SecTrustSetPolicies(serverTrust, SecPolicyCreateSSL(true, challenge.protectionSpace.host as CFString))
-            // get Evaluate server certificate result
-            var secresult = SecTrustResultType.invalid
-            SecTrustEvaluate(serverTrust, &secresult)
-            // Get remote cert data
-            let cFData = SecCertificateCopyData(secCertificate)
-            let cert = NSData(bytes: CFDataGetBytePtr(cFData), length: CFDataGetLength(cFData))
-            // check
-            if errSecSuccess == SecTrustEvaluate(serverTrust, &secresult), cert.isEqual(to: localCert) {
-                completionHandler(.useCredential, URLCredential(trust: serverTrust))
-            } else {
-                completionHandler(.cancelAuthenticationChallenge, nil)
+extension Network {
+    private class SessionDelegate: NSObject, URLSessionDelegate {
+        var sslPinning: Data?
+        /// MARK: URLSessionDelegate
+        func urlSession(_ session: URLSession, didBecomeInvalidWithError error: Error?) {}
+        func urlSession(_ session: URLSession, didReceive challenge: URLAuthenticationChallenge, completionHandler: @escaping (URLSession.AuthChallengeDisposition, URLCredential?) -> Swift.Void) {
+            guard let serverTrust = challenge.protectionSpace.serverTrust else {
+                completionHandler(URLSession.AuthChallengeDisposition.cancelAuthenticationChallenge, nil)
+                return
             }
-        } else {
-            if challenge.protectionSpace.authenticationMethod == NSURLAuthenticationMethodServerTrust {
-                completionHandler(.useCredential, URLCredential(trust: serverTrust))
+            if let localCert = sslPinning, let secCertificate = SecTrustGetCertificateAtIndex(serverTrust, 0) {
+                // Set SSL policies for domain name check
+                SecTrustSetPolicies(serverTrust, SecPolicyCreateSSL(true, challenge.protectionSpace.host as CFString))
+                // get Evaluate server certificate result
+                var secresult = SecTrustResultType.invalid
+                SecTrustEvaluate(serverTrust, &secresult)
+                // Get remote cert data
+                let cFData = SecCertificateCopyData(secCertificate)
+                let cert = NSData(bytes: CFDataGetBytePtr(cFData), length: CFDataGetLength(cFData))
+                // check
+                if errSecSuccess == SecTrustEvaluate(serverTrust, &secresult), cert.isEqual(to: localCert) {
+                    completionHandler(.useCredential, URLCredential(trust: serverTrust))
+                } else {
+                    completionHandler(.cancelAuthenticationChallenge, nil)
+                }
             } else {
-                completionHandler(.cancelAuthenticationChallenge, nil)
+                if challenge.protectionSpace.authenticationMethod == NSURLAuthenticationMethodServerTrust {
+                    completionHandler(.useCredential, URLCredential(trust: serverTrust))
+                } else {
+                    completionHandler(.cancelAuthenticationChallenge, nil)
+                }
             }
         }
+        #if !os(OSX)
+        func urlSessionDidFinishEvents(forBackgroundURLSession session: URLSession) {}
+        #endif
     }
-    #if !os(OSX)
-    public func urlSessionDidFinishEvents(forBackgroundURLSession session: URLSession) {}
-    #endif
 }
